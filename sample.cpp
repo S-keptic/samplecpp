9dea1da794e458e53113f873cbd743d39b893d10
Anmol <anmol.volt@gmail.com>
2024-05-27 18:03:33 +0530

    ncr

--- sample.cpp
+++ sample.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int iseven(int n){
-    if(n%2==0){
-        return true;
-    }else{
-        return false;
-    }
+int factorial(int n){
+    int fact = 1;
 
-}
-int main(){
-    bool ans = iseven(5);
-    if (ans==1){
-        cout <<"is even"; 
-    }else{
-        cout << "is odd";
+    for(int i = 1; i<=n;i++){
+        fact = fact *i;
     }
+    return fact;
+}
+
+int ncr(int n , int r){
+int num = factorial(n);
+int denom = factorial(r)*factorial(n-r);
+int factorial = num/denom;
+
+return factorial;
+}
+
+int main(){
+int n ,r ;
+cout<< "enter the value of n" <<endl;
+cin >> n;
+cout << "enter the value of r" << endl;
+cin >> r;
+
+int answer =ncr(n,r);
+
+cout << answer <<endl;
+
+
+
+
 }
