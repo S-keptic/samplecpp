3dfcbdd37d8292c3a31834c8f8ee42f5643d816d
Anmol <anmol.volt@gmail.com>
2024-06-03 23:19:45 +0530

    reverse array

--- sample.cpp
+++ sample.cpp
@@ -1,26 +1,25 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
+void reverseArray(int arr[],int size){
+    int start = 0;
+    int end = size-1;   
 
-void arraySum(int arr[] , int size){
-    int sum  = 0;
-    for(int i = 0 ; i < size ; i++){
-        sum = sum + arr[i]; 
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
     }
-    cout << sum <<endl; 
+    
 }
 
-
 int main(){
+    int myarray[5] = {1,2,3,4,5};
+    reverseArray(myarray,5);
 
-    int n ;
-    cin >>n;
 
-    int myarray[100];
 
-    for(int i = 0 ; i < n ; i++){
-        cin >> myarray[i];
-    } 
-
-    arraySum(myarray,n);
+    for(int i = 0 ;i < 5 ;i++){
+        cout << myarray[i] <<" " ;  
+    }
 }
