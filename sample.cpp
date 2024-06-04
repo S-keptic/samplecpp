26e56ae7220027005da3fb398a9b592b48204a53
Anmol <anmol.volt@gmail.com>
2024-06-04 23:12:35 +0530

    lol

--- sample.cpp
+++ sample.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
 using namespace std;
-
-void reverseArray(int arr[],int size){
-    int start = 0;
-    int end = size-1;   
-
-    while(start<=end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
+void swap(int arr[],int size){
+    for(int i = 0;i<size;i+=2){
+        if(i+1<size){
+            swap(arr[i],arr[i+1]);
+        }
     }
-    
 }
 
 int main(){
-    int myarray[5] = {1,2,3,4,5};
-    reverseArray(myarray,5);
-
-
-
-    for(int i = 0 ;i < 5 ;i++){
-        cout << myarray[i] <<" " ;  
+    int myarray[5] = {132 ,123 ,135 , 41 ,40};
+    swap(myarray,5);
+    for(int i = 0; i<5;i++){
+        cout <<myarray[i] << " "; 
     }
 }
