acda8c7376503245f1cdfc5af30db511e1082168
Anmol <anmol.volt@gmail.com>
2024-06-13 20:55:15 +0530

    fixed

--- sample.cpp
+++ sample.cpp
@@ -2,35 +2,29 @@
 using namespace std;
 
 
-int binarySearch(int arr[],int size,int key)
-{
+int binarySearch(int arr[],int size,int key){
     int start = 0;
     int end = size-1;
     int mid = (start+end)/2;
-
-    while(start<=end){
+     
+     while(start<=end){
         if(key==arr[mid]){
             return mid;
         }
         if(key>arr[mid]){
-            start = mid+1;
+            start = mid +1;
         }else{
-            end = mid-1;
+            end = mid - 1;
         }
         mid = (start+end)/2;
-
-    }
-    return -1;
+     }
+     return -1;
 }
-
-
 
 int main()
 {
-    int even[4] = {2,4,6,8};
-    int value = binarySearch(even,4,2);
-    cout << value <<endl;  
-
-
-
+    int even[6] = {2,4,6,8,10};
+    int odd[5] = {4,8,16,20,24};
+    int value = binarySearch(odd,5,20);
+    cout <<value <<endl;  
 }
