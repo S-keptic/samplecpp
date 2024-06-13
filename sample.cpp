bb94bab7ff45f34da167287bc1a64d50b85993c8
Anmol <anmol.volt@gmail.com>
2024-06-13 13:00:12 +0530

    binarySearch

--- sample.cpp
+++ sample.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 using namespace std;
-void swap(int arr[],int size){
-    for(int i = 0;i<size;i+=2){
-        if(i+1<size){
-            swap(arr[i],arr[i+1]);
+
+
+int binarySearch(int arr[],int size,int key)
+{
+    int start = 0;
+    int end = size-1;
+    int mid = (start+end)/2;
+
+    while(start<=end){
+        if(key==arr[mid]){
+            return mid;
         }
+        if(key>arr[mid]){
+            start = mid+1;
+        }else{
+            end = mid-1;
+        }
+        mid = (start+end)/2;
+
     }
+    return -1;
 }
 
-int main(){ 
-    int myarray[5] = {132 ,123 ,135 , 41 ,40};
-    swap(myarray,5);
-    for(int i = 0; i<5;i++){
-        cout <<myarray[i] << " "; 
-    }
-} 
+
+
+int main()
+{
+    int even[4] = {2,4,6,8};
+    int value = binarySearch(even,4,2);
+    cout << value <<endl;  
+
+
+
+}
