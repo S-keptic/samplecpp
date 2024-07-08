6bf27b4e6b147158e5adf0f543e936f7d3dbf3e3
Anmol <anmol.volt@gmail.com>
2024-07-08 19:56:38 +0530

    recursion

--- sample.cpp
+++ sample.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 using namespace std;
 
-
-int findPivot(int arr[],int size){
-    int start = 0;
-    int end = size-1;
-    int mid = start + (end-start)/2;
-
-
-    while(start<end){
-        if(arr[mid]<=arr[0]){
-            start = mid +1;
-
-        }else{
-            end = mid;
-        }
+int count = 1;
+void f(){
+    if(count == 101){
+        return;
     }
-    return start;
+    cout << count << endl;
+    count++;
+    f();
 }
 
-int main(){
-    int arr[6] = {1,7,3,6,5,6};
-    int val = findPivot(arr,6);
-    cout << val; 
-
+int main (){
+    f();
 }
