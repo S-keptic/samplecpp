ceb3ab75a313851a78303ccb7c31ae2d7fc2278c
Anmol <anmol.volt@gmail.com>
2024-07-09 12:11:30 +0530

    recursion

--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int count = 1;
-void f(){
-    if(count == 101){
+void f(int n){
+    if(count == n+1){
         return;
     }
     cout << count << endl;
     count++;
-    f();
+    f(n);
+
+
 }
 
-int main (){
-    f();
+int main(){
+    f(100);
 }
