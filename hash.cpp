470dfe393d81bbbc5cdb5609c91d970edfaecb24
Anmol <anmol.volt@gmail.com>
2024-09-04 01:37:46 +0530

    f

--- hash.cpp
+++ hash.cpp
@@ -1,24 +1,33 @@
+// C++ program for the implementation of Bubble sort
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n ;
-    cin >> n;
-    int arr[n];
-    for(int i =  0 ;  i<n ; i++  ){
-        cin >> arr[i];
-    } 
-    map<int , int> mpp;
-    for(int i  = 0 ; i <n ; i++ ){
-        mpp[arr[i]]++;
+void bubbleSort(vector<int>& v) {
+    int n = v.size();
 
-    }
-    int q;
-    cin >> q;
-    while(q--){
-        int number; 
-        cin >> number;
-        cout << mpp[number]; 
-    }
+    // Outer loop that corresponds to the number of
+    // elements to be sorted
+    for (int i = 0; i < n - 1; i++) {
 
+        // Last i elements are already
+        // in place
+        for (int j = 0; j < n - i - 1; j++) {
+          
+              // Comparing adjacent elements
+            if (v[j] > v[j + 1])
+              
+                  // Swapping if in the wrong order
+                swap(v[j], v[j + 1]);
+        }
+    }
+}
+
+int main() {
+    vector<int> v = {5, 1, 4, 2, 8};
+
+    // Sorting the vector v
+    bubbleSort(v);
+    for (auto i : v)
+        cout << i << " ";
+    return 0;
 }
--- sample.cpp
+++ sample.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int f(int n){
-    if(n<=1){
-        return n;
+int largest_element(int arr[],int n){
+    int largestElement = INT_MIN;
+    for(int i = 0; i<n;i++){
+        if(arr[i]>largestElement){
+            largestElement=arr[i];
+        }
+
+
     }
-    int last = f(n-1) ;
-    int secondLast = f(n-2);
-    return last + secondLast;  
+    return largestElement;
+}
 
-}   
-
-int main(){
-    cout << f(490);
+int main (){
+    int arr[] = {1,232 ,1341 ,3 , 13 ,13 ,435};
+    int answer = largest_element(arr,5);
+    cout <<answer; 
 }
